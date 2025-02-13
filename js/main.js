95b0d6a47fb71adb36bd6139d1c4e92ae7c9f682
dasha-deriglazova <deriglazovadaria27@gmail.com>
2025-02-13 12:44:50 +0400

    Открывается и закрывается

--- js/main.js
+++ js/main.js
@@ -3,4 +3,8 @@ import { getArrayPosts } from './create-array-manifest.js';
 
 import { createMiniatures } from './create-miniatures.js';
 
+import './create-big-picture.js';
+
 createMiniatures(structuredClone(getArrayPosts));
+
+
